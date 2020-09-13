bd3cab042eaa68f84fc6b15cff72743a1e0eaaed
Joey Wu <joeypwu@gmail.com>
2020-09-13 14:16:04 -0400

    Added DataGenerator to generate data to use. Added a prompt for user input for file names. Added a timer for CountingSort.

--- src/com/mitprimes/problem1/CountingSort.java
+++ src/com/mitprimes/problem1/CountingSort.java
@@ -3,21 +3,45 @@ package com.mitprimes.problem1;
 import com.mitprimes.Student;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class CountingSort {
 
-    //===========
-    // CONSTANTS
-    //===========
+    //===========//
+    // CONSTANTS //
+    //===========//
 
-    final static String INPUT_FILE = "input-problem1.txt";
+    final static String INPUT_FILE = "biginput-problem1.txt";
     final static String OUTPUT_FILE = "output-problem1.txt";
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter input file name: ");
+        String inputFile = input.nextLine();
+        System.out.print("Enter output file name: ");
+        String outputFile = input.nextLine();
+
+        if(inputFile.isEmpty()) inputFile = INPUT_FILE;
+        if(outputFile.isEmpty()) outputFile = OUTPUT_FILE;
+
+        File inputF = new File(inputFile);
+        File outputF = new File(outputFile);
+
+        if(!inputF.exists()){
+            System.out.println("The input file " + inputFile + " does not exist.");
+            main(args);
+        }
+        if(!outputF.exists()){
+            System.out.println("The output file " + outputFile + " does not exist.");
+            main(args);
+        }
+
+        long time;
+
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader(INPUT_FILE));
+            reader = new BufferedReader(new FileReader(inputF));
             String line = reader.readLine();
 
             int length = Integer.parseInt(line);
@@ -29,31 +53,39 @@ public class CountingSort {
                 String[] temp = line.split("\\s+");
                 temp[0] = temp[0].replace(",", "");
 
-                System.out.println(line);
                 inputArray[i] = new Student(temp[0], temp[1],
                         Float.parseFloat(temp[2]), Integer.parseInt(temp[3]));
             }
             reader.close();
 
+            final long startTime = System.currentTimeMillis();
             Student[] sortedArray = countingSort(inputArray);
+            final long endTime = System.currentTimeMillis();
 
-            FileWriter writer = new FileWriter(OUTPUT_FILE);
+            time = endTime - startTime;
+            System.out.println("Total execution time: " + (time));
+
+            FileWriter writer = new FileWriter(outputF);
+
+            writer.write(Integer.toString(length));
+            writer.write(System.getProperty("line.separator"));
 
             for(Student a : sortedArray) {
                 writer.write(a.toString());
                 writer.write(System.getProperty("line.separator"));
             }
 
+            writer.write(Long.toString(time));
+
             writer.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     private static Student[] countingSort(Student[] inputArray) {
+
 
         int length = inputArray.length;
         int range = 5;
@@ -65,8 +97,7 @@ public class CountingSort {
         for(int i = 0; i < range; i++)
             count[i] = 0;
 
-        for(int i = 0; i < length; i++)
-            count[inputArray[i].getGradYear()-2018]++;
+        for(Student student : inputArray) count[student.getGradYear() - 2018]++;
 
         for(int i = 1; i <= range-1; i++)
             count[i] += count[i-1];
@@ -75,6 +106,7 @@ public class CountingSort {
             output[count[inputArray[i].getGradYear()-2018]-1] = inputArray[i];
             --count[inputArray[i].getGradYear()-2018];
         }
+
 
         return output;
     }
--- src/com/mitprimes/problem1/DataGenerator.java
+++ src/com/mitprimes/problem1/DataGenerator.java
@@ -1,6 +1,95 @@
 package com.mitprimes.problem1;
 
+import com.mitprimes.Student;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+
 public class DataGenerator {
 
+    //===========//
+    // CONSTANTS //
+    //===========//
+
+    final static String BIG_INPUT = "biginput-problem1.txt";
+
+    public static void main(String[] args) {
+
+        try {
+            generateData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void generateData() throws IOException {
+
+
+        FileWriter writer = new FileWriter(BIG_INPUT);
+
+        int length = 10000000;
+
+        writer.write(Integer.toString(length));
+
+        writer.write(System.getProperty("line.separator"));
+
+        for(int i = 0; i < length; i++) {
+
+            String firstName = getRandomName();
+            String lastName = getRandomName();
+            float gpa = generateRandomGPA();
+            int gradYear = generateRandomGradYear();
+
+            Student billy = new Student(lastName, firstName, gpa, gradYear);
+            writer.write(billy.toString());
+            writer.write(System.getProperty("line.separator"));
+        }
+
+        writer.close();
+    }
+
+    private static int generateRandomGradYear() {
+
+        Random r = new Random();
+        int min = 2018;
+        int max = 2022;
+
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+
+        return randomNum;
+    }
+
+    private static float generateRandomGPA() {
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        Random r = new Random();
+        float min = 1;
+        float max = 4;
+
+        float result = min + r.nextFloat() * (max - min);
+        return Float.parseFloat(df.format(result));
+    }
+
+    private static String getRandomName() {
+        String chars = "abcdefghijklmnopqrstuvwxyz1ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder name = new StringBuilder();
+        Random rnd = new Random();
+
+        int randomLength = ThreadLocalRandom.current().nextInt(4, 12 + 1);
+
+        while (name.length() < randomLength) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * chars.length());
+            name.append(chars.charAt(index));
+        }
+        String toString = name.toString();
+        return toString;
+
+    }
 
 }
